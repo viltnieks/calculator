a968ca8ab08a3b760e10949d2ab2449957acb960
aaaaaaaaaaaaaaaaaaaa <90380498+viltnieks@users.noreply.github.com>
2022-05-19 08:37:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-function calculateSum() {
-    let skaitlis1 = document.getElementById('number1').value
-    let skaitlis2 = document.getElementById('number2').value
-    let sum = parseFloat(skaitlis1) + parseFloat(skaitlis2)
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelector("#result-box");
+let clearBtn = document.querySelector('#clear');
 
-    let element = document.getElementById('result')
-    element.innerHTML = sum
+let total = document.querySelector("#total");
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+
+// For Number Inputs
+btnSpread.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    // Inner Values for calculator
+
+    if (resultBox.innerHTML == "0") {
+      resultBox.innerHTML = "";
+    }
+
+    let value = btns[i].innerHTML;
+    resultBox.innerHTML += value;
+  });
+});
+
+// Function to evalute Strings
+function evaluate(fn) {
+    return new Function('return ' + fn)();
 }
+
+// To calculate All Input
+total.addEventListener('click', ()=> {
+let allInputs = resultBox.innerHTML;
+
+resultBox.innerHTML = evaluate(allInputs);
+
+console.log(evaluate(allInputs));
+})
+
+// Clear all Inputs
+clearBtn.addEventListener('click', ()=> {
+    resultBox.innerHTML = "0";
+})
